221040d76a21cc0d3d36390ef7343dc81ebf1745
Oblyvion <weber_f-j@live.de>
2018-12-03 00:38:40 +0100

    init project with express

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+
+http.createServer((req, res) => {
+    console.log('My http-Server started....');
+    console.log(req.url);
+
+    const header = 'Hello Server';
+
+    // Root-Page
+    if (req.url === '/') {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.end(header);
+    }
+    // API
+    if (req.url === '/api') {
+        res.writeHead(200, {'content-type': 'application/json'});
+        const json = JSON.stringify({
+            Header: header,
+            'TODO': "Eventuell schon mal eine 'kleine' Datenbank erstellen, mh?"
+        });
+        res.end(json);
+    }
+    if (req.url === '/missing') {
+        res.writeHead(404, {'content-type' : 'text/plain'});
+        res.end('Here you can find nothing ;)');
+    }
+}).listen(3000);
